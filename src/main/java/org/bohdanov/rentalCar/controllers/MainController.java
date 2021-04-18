680b79d3d893ea2dce738a0a00ec475db51e2c4b
VadymSula <sulawados312000@gmail.com>
2021-04-18 16:34:54 +0300

    fix: deploy v1 add CORS

--- src/main/java/org/bohdanov/rentalCar/controllers/MainController.java
+++ src/main/java/org/bohdanov/rentalCar/controllers/MainController.java
@@ -5,9 +5,11 @@ import org.bohdanov.rentalCar.mappers.CustomMapper;
 import org.bohdanov.rentalCar.storage.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class MainController {
 
