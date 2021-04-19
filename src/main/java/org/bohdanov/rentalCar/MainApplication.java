11a06a43e599ea448610e2dce18b88e72e948dbe
VadymSula <sulawados312000@gmail.com>
2021-04-19 11:10:29 +0300

    fix: deploy v1 fixed CORS

--- src/main/java/org/bohdanov/rentalCar/MainApplication.java
+++ src/main/java/org/bohdanov/rentalCar/MainApplication.java
@@ -1,6 +1,5 @@
 package org.bohdanov.rentalCar;
 
-import org.bohdanov.rentalCar.configs.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,9 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-        FileStorageProperties.class
-})
+//@EnableConfigurationProperties({
+//        FileStorageProperties.class
+//})
 @EnableSwagger2
 public class MainApplication {
     public static void main(String[] args) {
