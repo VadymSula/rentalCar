0245030b4285ded448f50086b419eb9bc694e381
VadymSula <sulawados312000@gmail.com>
2021-04-25 23:28:11 +0300

    deploy: v3.1 : fix5: cors

--- src/main/java/org/bohdanov/rentalCar/configs/CorsFilter.java
+++ src/main/java/org/bohdanov/rentalCar/configs/CorsFilter.java
@@ -23,8 +23,8 @@ public class CorsFilter implements Filter, WebMvcConfigurer {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        System.out.println("WebConfig; "+request.getRequestURI());
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        System.out.println("WebConfig; " + request.getRequestURI());
+        response.setHeader("Access-Control-Allow-Origin", "https://rental-car-ua.netlify.app");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
         response.setHeader("Access-Control-Max-Age", "3600");
@@ -34,20 +34,20 @@ public class CorsFilter implements Filter, WebMvcConfigurer {
         response.addHeader("Access-Control-Expose-Headers", "ROLE");
         response.addHeader("Access-Control-Expose-Headers", "responseType");
         response.addHeader("Access-Control-Expose-Headers", "observe");
-        System.out.println("Request Method: "+request.getMethod());
+        System.out.println("Request Method: " + request.getMethod());
         if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
             try {
                 chain.doFilter(req, res);
-            } catch(Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         } else {
             System.out.println("Pre-flight");
-            response.setHeader("Access-Control-Allow-Origin", "https://rental-car-ua.netlify.app/");
+            response.setHeader("Access-Control-Allow-Origin", "https://rental-car-ua.netlify.app");
             response.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT");
             response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers", "Access-Control-Expose-Headers"+"Authorization, content-type," +
-                    "USERID"+"ROLE"+
+            response.setHeader("Access-Control-Allow-Headers", "Access-Control-Expose-Headers" + "Authorization, content-type," +
+                    "USERID" + "ROLE" +
                     "access-control-request-headers,access-control-request-method,accept,origin,authorization,x-requested-with,responseType,observe");
             response.setStatus(HttpServletResponse.SC_OK);
         }
