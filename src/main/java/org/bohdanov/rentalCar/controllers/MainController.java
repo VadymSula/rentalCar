63bf04023db24de85298f42708d62e8354474d26
VadymSula <sulawados312000@gmail.com>
2021-04-26 13:15:57 +0300

    deploy: v3.1 : fix12: cors

--- src/main/java/org/bohdanov/rentalCar/controllers/MainController.java
+++ src/main/java/org/bohdanov/rentalCar/controllers/MainController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 @RestController
 public class MainController {
 
