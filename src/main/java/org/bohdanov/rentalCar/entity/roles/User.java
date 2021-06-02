374322d9d4e178c05923ea653abe959b4a600db6
VadymSula <sulawados312000@gmail.com>
2021-06-02 13:00:29 +0300

    deploy: v3.3 : fixed: deleted cascades

--- src/main/java/org/bohdanov/rentalCar/entity/roles/User.java
+++ src/main/java/org/bohdanov/rentalCar/entity/roles/User.java
@@ -27,7 +27,7 @@ public class User implements UserDetails {
     private boolean isRentRole;
     @Transient
     private JwtResponse token;
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles;
 
     @JsonIgnore
